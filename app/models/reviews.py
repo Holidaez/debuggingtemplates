209110497036766a0121d9169d83a620b990b6fb
Holidaez <holidaez@DESKTOP-GG36629>
2023-07-20 17:31:50 -0400

    fixed

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -9,5 +9,5 @@ class Review(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
     business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'), nullable=False)
 
-    user = db.relationship("User", back_populates="reviews")
-    businesses = db.relationship("Businesses", back_populates="reviews")
+    user = db.relationship("User", back_populates=reviews)
+    businesses = db.relationship("Businesses", back_populates=reviews)
